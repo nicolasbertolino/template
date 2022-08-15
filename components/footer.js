ca5c0f341da0b7da6938b4d11a5f9f1fe682f0ff
Nicolas Bertolino <bertolinonicolas@gmail.com>
2022-08-15 19:09:20 +0200

    Init

---
+++ components/footer.js
@@ -0,0 +1,19 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <footer class="footer">
+        <ul>
+          <li><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', Footer);

17538946893a3d3ca6f43f7f98267e57676286ec
Nicolas Bertolino <bertolinonicolas@gmail.com>
2022-09-24 12:17:54 +0200

    do some styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
 /* Components */
-import footerComponent from "../views/components/footer-component.js";
-import headerComponent from "../views/components/header-component.js";
+import footerComponent from '../views/components/footer-component.js';
+import headerComponent from '../views/components/header-component.js';
 
 /* Components Logic */
-import footer from "./components/footer.js";
-import header from "./components/header.js";
+import footer from './components/footer.js';
+import header from './components/header.js';
 
 /* Main */
-import main from "./main/main.js";
+import main from './main/main.js';
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", init);
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', init);
 } else {
   init();
 }
 
 function init() {
-  footer.init();
   footerComponent.init();
-  header.init();
+  footer.init();
   headerComponent.init();
+  header.init();
   main.init();
 }
 

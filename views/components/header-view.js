bc5109fa56a3e49dad089b05a0c30ee66cf2c8d9
Nicolas Bertolino <bertolinonicolas@gmail.com>
2025-08-20 12:41:23 +0200

    Update header-view.js

--- views/components/header-view.js
+++ views/components/header-view.js
@@ -2,12 +2,22 @@ function init() {
   const template = document.createElement('template');
   template.innerHTML = `
   <header class="header">
-    <div class="header__logo">Logo</div>
+    <div class="header__logo">
+      <svg class="icon" viewBox="0 0 24 24">
+        <use href="/assets/icons/sprite.svg#logo"></use>
+      </svg>
+    </div>
     <nav class="header__nav">
       <ul>
-        <li><a href="about.html">About</a></li>
-        <li><a href="services.html">Services</a></li>
-        <li><a href="contact.html">Contact</a></li>
+        <li>
+          <a href="about.html">About</a>
+        </li>
+        <li>
+          <a href="services.html">Services</a>
+        </li>
+        <li>
+          <a href="contact.html">Contact</a>
+        </li>
       </ul>
     </nav>
   </header>
